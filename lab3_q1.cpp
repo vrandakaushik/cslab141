cfa319cb046e29ba6a0f7da781a2f156a5478ecd
vrandakaushik <42139229+vrandakaushik@users.noreply.github.com>
2018-08-16 20:17:40 +0530

    Add files via upload

---
+++ lab3_q1.cpp
@@ -0,0 +1,20 @@
+ // first include the lbrary
+#include <iostream>
+using namespace std;
+//write the main function
+int main(){
+// addition of two numbers
+ int sum = 9+2;
+// display the result in terminal
+cout << sum <<endl;
+// writing a decimal number as a sum of two numbers using float
+float a = 100+0.99; cout << a << endl;cout<<"size of a="<< sizeof(a)<<endl;
+// using char for printing a word in terminal
+char z[]="vranda"; cout<< z << endl; cout<<"size of z="<< sizeof(z)<<endl;
+// using boolean for defining true or flase for a statement
+bool b= true; cout<< b<<endl;
+bool iAmGoodAtSinging = false; cout << iAmGoodAtSinging<<endl;
+cout<< "size of sum=" << sizeof(sum)<<endl;
+}
+
+
