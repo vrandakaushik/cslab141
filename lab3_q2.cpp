51b1d9c27549be2e911842111848eb9c91ecb483
vrandakaushik <42139229+vrandakaushik@users.noreply.github.com>
2018-08-16 20:24:58 +0530

    Add files via upload

---
+++ lab3_q2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+int a=77 ,b= 84;
+int sum= a+b; cout<<sum<<endl;
+int difference = a-b; cout<<difference<<endl;
+int multiply=a*b; cout<< multiply<<endl;
+int divide=a/b;cout<<divide<<endl;
+int mod=a%b;cout<<mod<<endl;
+}
